a612ba61ef26022945165e7a31865dd743fee35b
Moosems <95927277+Moosems@users.noreply.github.com>
2023-04-11 07:59:28 -0400

    Ach no! No test lol

--- test.py
+++
@@ -1,9 +0,0 @@
-from chlorophyll import CodeView
-from tkinter import Tk
-
-root = Tk()
-cd = CodeView(root)
-cd.pack()
-cd.insert("end", "\"\"\"\nThis is a test\n\n\n\n\"\"\"")
-cd.insert("end", "\n\nprint()")
-root.mainloop()
